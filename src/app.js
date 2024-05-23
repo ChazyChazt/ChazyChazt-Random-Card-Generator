940c697554924d74c072fabec932fb17e47ad360
ChazyChazt <147471127+ChazyChazt@users.noreply.github.com>
2024-05-23 22:26:07 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,51 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
+  function aleatoriNumber() {
+    let arraynumber = [
+      "A",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "J",
+      "Q",
+      "K"
+    ];
+    let randomnumber = Math.floor(Math.random() * arraynumber.length);
+    return arraynumber[randomnumber];
+  }
+
+  function aleatoriIcon() {
+    let arrayIcon = ["♦", "♥", "♠", "♣"];
+    let randomIcon = Math.floor(Math.random() * arrayIcon.length);
+    return arrayIcon[randomIcon];
+  }
+
   //write your code here
+  const iconUp = document.getElementById("iconUp");
+  const numberElement = document.getElementById("number");
+  const iconDown = document.getElementById("iconDown");
+
+  const number = aleatoriNumber();
+  const icon = aleatoriIcon();
+
+  let color = "black";
+  if (icon === "♦" || icon === "♥") {
+    color = "red";
+  }
+
+  numberElement.innerHTML = number;
+  iconUp.innerHTML = icon;
+  iconDown.innerHTML = icon;
+
+  iconUp.style.color = color;
+  iconDown.style.color = color;
+
   console.log("Hello Rigo from the console!");
 };
